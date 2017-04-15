383f801694099b2d186b0fc34847d637294cd278
Frank Winstead <frank.winstead@gmail.com>
2017-04-15 14:51:30 -0400

    fix RUBY_VER

--- misc/download.sh
+++ misc/download.sh
@@ -10,7 +10,6 @@
 DOWNLOAD_DIR="${OPENSHIFT_REPO_DIR}downloads"
 if [ \! -d "${DOWNLOAD_DIR}" ] ; then mkdir -vp "${DOWNLOAD_DIR}"; fi
 
-export RUBY_VER="2.3.4"
 TO_DOWNLOAD=("https://cache.ruby-lang.org/pub/ruby/2.3/ruby-${RUBY_VER}.tar.gz")
 
 # Retrieve files
